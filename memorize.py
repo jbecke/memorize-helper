ab07b713e2d87aa90f25f46750a87b17221e3f9e
Jacob Beckerman <jacob.beckerman@gmail.com>
2017-05-29 17:03:06 -0400

    added retraining, error checking

--- memorize.py
+++ memorize.py
@@ -14,44 +14,79 @@ import fileinput
 
 file = sys.argv[1]
 rflag = int(sys.argv[2])
-wrong_list = set([])
+to_test = {}
 
+# Get everything into a dictionary
 for line in fileinput.input(file):
 	tokenized = line.split(",")
 	key = tokenized[0].strip()
 	value = tokenized[1].strip()
 	if len(tokenized) > 2:
 		print("ERROR: Only one comma per line")
-	elif rflag == 0:
-		while True:
-			print(key)
-			guess = input().strip()
-			if guess == value:
-				print("_/\n\n")
-				break
-			elif guess == 'q':
-				print("X\n\n")
-				wrong_list.add(key)
-				break
-			else:
-				wrong_list.add(key)
-	elif rflag == 1:
-		while True:
-			print(value)
-			guess = input().strip()
-			if guess == key:
-				print("_/\n\n")
-				break
-			elif guess == 'q':
-				print("X\n\n")
-				wrong_list.add(key)
-				break
-			else:
-				wrong_list.add(value)
-	else:
-		print("ERROR: reverse flag must be 0 or 1")
-		exit()
+		exit(1)
+	to_test[key] = value
 
-for i in wrong_list:
+def test(to_test):
+	next_test = {}
+	wrong_list = set([])
+	for key, value in to_test.items():
+		if rflag == 0:
+			while True:
+				print(key)
+				guess = input().strip()
+				if guess == value:
+					print("_/\n\n")
+					break
+				elif guess == 'q':	# give up
+					print("X\n\n")
+					wrong_list.add(key)
+					next_test[key] = value
+					break
+				elif guess == 'a':	# give answer
+					print(value)
+				elif guess == 'c':	# accept answer as right
+					print("_/\n\n")
+					break
+				elif guess == 'exit':	# accept answer as right
+					print("\n\nDone! The ones you got wrong were:")
+					for i in wrong_list:
+						print(i)
+					exit()
+				else:
+					wrong_list.add(key)
+					next_test[key] = value
+		elif rflag == 1:
+			while True:
+				print(value)
+				guess = input().strip()
+				if guess == key:
+					print("_/\n\n")
+					break
+				elif guess == 'q':
+					print("X\n\n")
+					wrong_list.add(value)
+					next_test[key] = value
+					break
+				elif guess == 'a':
+					print(key)
+				elif guess == 'c':
+					print("_/\n\n")
+					break
+				elif guess == 'exit':	# accept answer as right
+					print("\n\nDone! The ones you got wrong were:")
+					for i in wrong_list:
+						print(i)
+					exit()
+				else:
+					wrong_list.add(value)
+					next_test[key] = value
+
 	print("\n\nDone! The ones you got wrong were:")
-	print(i)
+	for i in wrong_list:
+		print(i)
+	print("\n\n")
+	if len(next_test) != 0:
+		test(next_test)
+
+
+test(to_test)
